034de2221c2271984cc87131b64c6d49ce2609a8
DeltaNetworks <41640896+DeltaNetworks@users.noreply.github.com>
2025-07-06 23:14:26 +0300

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -5,14 +5,6 @@ class HomePage extends BaseUtils
     login(username, password)
     {
         cy.visit(this.amazonUrl)
-        cy.wait(500)
-        //cy.get('body').then($body => {
-        //const button = $body.find('.a-button-text')
-        //if (button.length > 0 && button.is(':visible')) 
-            //{ 
-                //cy.get('.a-button-text').click()
-            //}
-        //})
         cy.get('#nav-link-accountList-nav-line-1').trigger('mouseover')
         cy.contains('span.nav-action-inner', 'Sign in').click()
         cy.get('#ap_email_login, #ap_email').clear().type(username)
